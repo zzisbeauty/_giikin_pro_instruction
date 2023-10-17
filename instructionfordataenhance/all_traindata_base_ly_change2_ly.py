17c0e8ce12688da1b022fb2399271210eb9290a6
swhardbewell@qq.com <swhardbewell@qq.com>
2023-10-17 10:39:41 +0800

    生成了BELLEversion2的训练数据，同时也生成了LY的训练数据，但是LY的训练是失败的

--- instructionfordataenhance/all_traindata_base_ly_change2_ly.py
+++ instructionfordataenhance/all_traindata_base_ly_change2_ly.py
@@ -1,5 +1,5 @@
 # todo 将 LY 格式数据（这个格式也是标注的格式）转化为BL需要的格式数据
-import json, random
+import json, random, jsonlines
 
 # # LY format data
 # # todo change LY data 2 BL data and save data with NELLE format   -  jsonl
@@ -19,7 +19,14 @@ with open('sdtconvdatahuman_1007.json', 'r', encoding='utf-8') as f:
 
 lines = lines_human + lines
 
-# # todo save data for LY
+# # todo save data for LY  json file  ==========================>  IMPORTANT： 重要最全新的数据，相较于上面的 lines，这个已经持久化的文件已经去掉了一些错误文件
 # random.shuffle(lines)
 # with open('all_train_human_enhancedata_all_ly.json', 'w', encoding='utf-8') as f:
 #     json.dump(lines, f, indent=4, ensure_ascii=False)
+
+# todo 存储完全正确的 all_train_human_enhancedata_all_ly.json 数据文件，存储为 jsonl 文件，方便 ly 训练
+with open('all_train_human_enhancedata_all_conv.batch.1.json', 'r', encoding='utf-8') as f:
+    lines = json.load(f)
+with jsonlines.open('all_train_human_enhancedata_all_ly.jsonl', mode='a') as writer:
+    for line in lines:
+        writer.write(line)
